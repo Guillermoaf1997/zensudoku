dd5a1fa0b82a4417d901610877f3b358ad55a4c2
Guillermoaf1997 <165324759+Guillermoaf1997@users.noreply.github.com>
2026-05-12 12:11:30 +0200

    Add files via upload

---
+++ sw.js.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'zen-sudoku-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Devuelve el archivo del caché si existe, si no, lo descarga de internet
+        return response || fetch(event.request);
+      })
+  );
+});
